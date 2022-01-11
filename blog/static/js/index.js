8614c11b0d7e759c8879c3f59750f8937aa389b6
Ghjattu <icracked369@gmail.com>
2022-01-11 21:42:21 +0800

    upgrade index

---
+++ blog/static/js/index.js
@@ -0,0 +1,10 @@
+// document.onreadystatechange = function () {
+//     if (document.readyState === 'complete') {
+//         console.log('complete');
+//     }
+// };
+window.onload = function () {
+    // console.log('onload');
+    let loading = document.getElementsByClassName('loading')[0];
+    loading.style.display = 'none';
+};
