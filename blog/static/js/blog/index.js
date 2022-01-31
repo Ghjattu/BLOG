4f1fad8e1a82248d61b1d8267cbe5422b96f6613
Ghjattu <icracked369@gmail.com>
2022-01-31 11:26:22 +0800

    updated the style of the index page

--- blog/static/js/blog/index.js
+++ blog/static/js/blog/index.js
@@ -12,34 +12,4 @@ $(function () {
             window.scrollTo({"behavior": "smooth", "top": anchorLocation.offsetTop});
         }
     });
-
-    Particles.init({
-        selector: '.bg-particles',
-        // 1201px ~
-        maxParticles: 70,
-        connectParticles: true,
-
-        responsive: [
-            {
-                // 800px ~ 1200px
-                breakpoint: 1200,
-                options: {
-                    maxParticles: 60,
-                }
-            }, {
-                // 481px ~ 799px
-                breakpoint: 800,
-                options: {
-                    maxParticles: 50,
-                }
-            }, {
-                // 0 ~ 480px
-                breakpoint: 480,
-                options: {
-                    maxParticles: 50,
-                    connectParticles: false,
-                }
-            }
-        ]
-    });
 });
